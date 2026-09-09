0b9d57e4a42181304ddc5f42493e1c8ee7909792
Snoww-jk <fr.aguirrer@duocuc.cl>
2026-09-09 10:48:24 -0300

    Configurar microservicios y conexion del BFF

--- urban-kicks-bff/src/main/java/com/urbankicks/bff/BffController.java
+++ urban-kicks-bff/src/main/java/com/urbankicks/bff/BffController.java
@@ -1,10 +1,13 @@
 package com.urbankicks.bff;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 
 @RestController
 @RequestMapping("/api")
 public class BffController {
+
+    private final RestClient restClient = RestClient.create();
 
     @GetMapping("/public/hello")
     public String hello() {
@@ -19,5 +22,31 @@ public class BffController {
     @GetMapping("/admin/hello")
     public String adminHello() {
         return "Acceso administrador";
+    }
+
+    @GetMapping("/catalog/products")
+    public String products() {
+        return restClient.get()
+                .uri("http://localhost:8082/products")
+                .retrieve()
+                .body(String.class);
+    }
+
+    @PostMapping("/orders")
+    public String createOrder(@RequestBody String body) {
+        return restClient.post()
+            .uri("http://localhost:8081/orders")
+            .header("Content-Type", "application/json")
+            .body(body)
+            .retrieve()
+            .body(String.class);
+    }
+
+    @GetMapping("/orders")
+    public String getOrders() {
+        return restClient.get()
+            .uri("http://localhost:8081/orders")
+            .retrieve()
+            .body(String.class);
     }
 }
