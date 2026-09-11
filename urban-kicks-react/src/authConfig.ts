f5be37513e71da8f4ec2227e1a22e813fa1014b0
Snoww-jk <fr.aguirrer@duocuc.cl>
2026-09-11 20:49:59 -0300

    Corregir configuracion de Cognito en React

--- urban-kicks-react/src/authConfig.ts
+++ urban-kicks-react/src/authConfig.ts
@@ -3,9 +3,9 @@ import type { AuthProviderProps } from "react-oidc-context";
 export const cognitoAuthConfig: AuthProviderProps = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_VuVX3Pv5a",
   client_id: "7g1pk0kvuigi2ftb3bq3f18u5n",
-  redirect_uri: "http://localhost:5174",
+  redirect_uri: "http://localhost:5173",
   response_type: "code",
-  scope: "email openid phone",
+  scope: "email openid",
   onSigninCallback: () => {
     window.history.replaceState(
       {},
