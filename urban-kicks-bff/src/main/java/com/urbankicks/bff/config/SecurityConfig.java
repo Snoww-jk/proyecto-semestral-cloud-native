255bbd4e041af692be39fc4b0d29f5e6d51a4ad8
Snoww-jk <fr.aguirrer@duocuc.cl>
2026-09-11 23:09:42 -0300

    Permitir DELETE en CORS del BFF

--- urban-kicks-bff/src/main/java/com/urbankicks/bff/config/SecurityConfig.java
+++ urban-kicks-bff/src/main/java/com/urbankicks/bff/config/SecurityConfig.java
@@ -144,6 +144,7 @@ public class SecurityConfig {
                         "GET",
                         "POST",
                         "PATCH",
+                        "DELETE",
                         "OPTIONS"
                 )
         );
